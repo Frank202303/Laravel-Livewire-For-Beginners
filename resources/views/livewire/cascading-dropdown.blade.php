78aabde9332c537aea17a1b28f06e3cc45b63c31
frankzhang2022 <frank.zhang@pixelforce.com.au>
2023-07-11 16:41:33 +1000

    1. finish component; 2. finish view: has bug, loading not override  country selection;

--- resources/views/livewire/cascading-dropdown.blade.php
+++ resources/views/livewire/cascading-dropdown.blade.php
@@ -1,3 +1,22 @@
-<div>
-    {{-- Success is as dangerous as failure. --}}
+<div >
+    <div class="flex flex-col gap-6 max-w-[400px] mx-auto py-16 justify-center items-center">
+        <select wire:model='selectedContinent' wire:change='changeContinent'  style="width: 400px;">
+            <option value="-1"> Please select continent</option>
+            @foreach ($continents as $continent)
+                 <option value ="{{$continent->id}}"> {{$continent->name}}</option>
+            @endforeach
+        </select>
+
+        <div class="flex relative">
+            <p wire:loading class="absolute  left-0 top-0 right-0 bottom-0 z-10 bg-white bg-opacity-90 py-2 px-3 "> 
+                Loading...
+            </p>
+            <select wire:model='selectedCountry'  class="flex-1"  style="width: 400px;">
+                <option value="-1"> Please select country</option>
+                @foreach ($countries as $country)
+                    <option value="{{$country->id}}"> {{$country->name}}</option>
+                @endforeach
+            </select>
+        </div>
+    </div> 
 </div>
