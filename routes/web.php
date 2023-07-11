0d1b8209780aded4bc2d5c5e85e6d7112ef71e90
frankzhang2022 <frank.zhang@pixelforce.com.au>
2023-07-11 14:43:32 +1000

    1 use seeder generate mock data 2. add Route 3. add Component and Model 4. Country Factory: mock countries 5. add Blade;

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Livewire\Calculator;
+use App\Http\Livewire\CascadingDropdown;
 use App\Http\Livewire\TodoList;
 use Illuminate\Support\Facades\Route;
 
@@ -30,3 +31,6 @@ Route::get('/calculator', Calculator::class,
 )->name('calculator');
 
 Route::get('/todo-list', TodoList::class,)->name('todo-list');
+Route::get('/cascading-dropdown', CascadingDropdown::class,)->name('cascading-dropdown');
+
+
