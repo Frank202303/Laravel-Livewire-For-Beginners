f7cccd8138f74b02dba5b9fa90adb8811396f729
frankzhang2022 <frank.zhang@pixelforce.com.au>
2023-07-16 16:26:55 +1000

    1. function is ok. 2 view not center, pagination style need to optimize

--- app/Http/Livewire/ProductSearch.php
+++ app/Http/Livewire/ProductSearch.php
@@ -2,12 +2,41 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ProductSearch extends Component
 {
+    // 分页
+    use WithPagination;
+    public string $search = '';
+
+    // watch query string searched in the URL
+    protected $queryString = ['search'] ;
+
     public function render()
     {
-        return view('livewire.product-search');
+        $query = Product::query();
+        // 或者  或者
+        if($this->search){
+            //                            Fuck: "%{$this->search}%"  must use ""
+            $query->where('title','like', "%{$this->search}%")
+            ->orWhere('description','like', "%{$this->search}%");
+        }
+        // pass parameter
+        return view('livewire.product-search',[
+            'products'=>$query->paginate(20),
+        ]);
+    }
+    // hook 生命周期钩子
+    public function updated($property)
+    {
+        // when search property updated
+        if($property =='search'){
+            // resetPage from pagination
+            $this->resetPage();
+        }
+
     }
 }
--- resources/views/livewire/product-search.blade.php
+++ resources/views/livewire/product-search.blade.php
@@ -1,3 +1,31 @@
-<div>
-    {{-- Be like water. --}}
+
+<div class="container mx-auto py-16 px-8">
+    <div class="mb-4">
+        {{-- lazy: 不是onchange，而是按下enter 或者点击搜索时： 才call API --}}
+        <input type="text" wire:model.lazy='search' placeholder="Search for Products">
+    
+    </div>
+    <table class="table-auto w-full">
+        <thead>  
+            <tr>
+                <th class="py-2 px-3 bg-gray-100  border-b-2 text-left"> ID </th>
+                <th class="py-2 px-3 bg-gray-100  border-b-2 text-left"> Image </th>
+                <th class="py-2 px-3 bg-gray-100  border-b-2 text-left"> Title </th>
+                <th class="py-2 px-3 bg-gray-100  border-b-2 text-left"> Description </th>
+            </tr>
+        </thead>
+        <tbody>  
+            {{-- products: passed by  return view('livewire.product-search',['products'=>$query->paginate(20), ]) --}}
+            @foreach ($products as $product)
+                <tr> 
+                    <td class="py-2 px-3  border-b ">{{$product->id}}</td>
+                    <td class="py-2 px-3  border-b "> <img  class='w-16'  src="{{$product->image}}"></td>
+                    <td class="py-2 px-3  border-b ">{{$product->title}}</td>
+                    <td class="py-2 px-3  border-b ">{{$product->price}}</td>
+                </tr>
+            @endforeach
+        </tbody>  
+    </table>
+    {{$products->links()}}
+    
 </div>
