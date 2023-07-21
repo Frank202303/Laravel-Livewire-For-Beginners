be65ea494a4c32c4fee0c7393babd7ea503ed0c6
frankzhang2022 <frank.zhang@pixelforce.com.au>
2023-07-21 17:00:08 +1000

    add comment for imageUpload

--- app/Http/Livewire/ImageUpload.php
+++ app/Http/Livewire/ImageUpload.php
@@ -16,13 +16,15 @@ class ImageUpload extends Component
     public function render()
     {
         return view('livewire.image-upload',[
-            // pass params, only pass image format
+            // pass params, only pass image format。
+            // 读取所有文件系统的文件，然后过滤出图片，把图片传参给view
             'images'=>collect(Storage::files('public'))
             ->filter(function($file){
                 return in_array(strtolower(pathinfo($file,PATHINFO_EXTENSION)),['png','jpg','jpeg','gif','webp']);
             })
             // map file to url
             ->map(function($file){
+                // map成url，这样在前端检查元素时，每个图片就有src=***url了
                 return Storage::url($file);
             })
         ]);
@@ -34,6 +36,7 @@ class ImageUpload extends Component
             'image.*'=>'image|max:1024',// 1MB Max
         ]);
         // store to public file with  Temporary name
+        // 可以一次上传多个图片
         foreach($this->image as $image){
             $image->store('public');
         }
--- resources/views/livewire/image-upload.blade.php
+++ resources/views/livewire/image-upload.blade.php
@@ -1,5 +1,6 @@
 <div class="p-6">
    <form wire:submit.prevent='save' class="flex flex-col w-[400px] mx-auto py-16"> 
+    {{-- 选择图片后，就会把图片保存在临时位置，点击 save时，把它们保存在永久位置 --}}
         {{-- 展示 model='image' --}}
         @if($image)
             Preview:
@@ -23,6 +24,7 @@
    </form>
 
    {{-- /// for existed images --}}
+   {{-- C:\xampp\htdocs\laravel10\Laravel-Livewire-For-Beginners\public\storage --}}
     <div class="flex  flex-wrap gap-7">
             {{-- //这个 images 是通过 view传 参数过来的 --}}
             @foreach ($images as $image)
